04a6b18abae317f7ed4d3e38783910811170e6f7
dylanperer <dylan.perer29@protonmail.com>
2023-10-20 21:02:01 +1300

    .

--- Application/Authentication/ValidationConstants.cs
+++ Application/Authentication/ValidationConstants.cs
@@ -3,6 +3,6 @@
 public static class ExceptionsConstants
 {
     public const string SignInInvalidCredentials = "Sorry, invalid credentials. Please try again.";
-    public const string SignUpDuplicate = "Sorry, account already exits. Please try again.";
+    public const string SignUpDuplicate = "Sorry, account already exits. Please sign in.";
     public const string InvalidRefreshToken = "Invalid refresh token.";
 }
