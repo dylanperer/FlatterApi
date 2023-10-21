69f36c19dc6642bbc7e3daf3d044043ff665ff75
dylanperer <dylan.perer29@protonmail.com>
2023-10-21 13:25:40 +1300

    .

---
+++ Application/Profile/Queries/GetGenderIdentitiesQuery.cs
@@ -0,0 +1,39 @@
+﻿// using Application.Dtos;
+// using Application.External.Interfaces.Authentication;
+// using Application.Mappers;
+// using LanguageExt.Common;
+// using MediatR;
+// using Microsoft.EntityFrameworkCore;
+// using Persistence.PostgresSql;
+//
+// namespace Application.Profile.Queries;
+//
+// public struct GetGenderIdentitiesQuery : IRequest<Result<ProfileDto>>
+// {
+//     public class GetGenderIdentitiesQueryHandler : IRequestHandler<GetProfileByIdQuery, Result<ProfileDto>>
+//     {
+//         private readonly PostgresDbContext _postgresDbContext;
+//         private readonly IUserProvider _userProvider;
+//
+//         public GetGenderIdentitiesQueryHandler(PostgresDbContext postgresDbContext, IUserProvider userProvider)
+//         {
+//             _postgresDbContext = postgresDbContext;
+//             _userProvider = userProvider;
+//         }
+//
+//         public async Task<Result<ProfileDto>> Handle(GetProfileByIdQuery request,
+//             CancellationToken cancellationToken)
+//         {
+//             var profile =
+//                 await _postgresDbContext.Profiles.FirstOrDefaultAsync(c => c.ProfileId == request._userId,
+//                     cancellationToken: cancellationToken);
+//
+//             if (profile == null)
+//             {
+//                 return new KeyNotFoundException().ToResult<ProfileDto>();
+//             }
+//             
+//             return new ProfileResultMapper().Map(profile);
+//         }
+//     }
+// }
---
+++ Persistence/Migrations/20231020235250_t2.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class t2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "Profiles");
+
+            migrationBuilder.DropColumn(
+                name: "IGenderIdentityId",
+                table: "Profiles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "Profiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "IGenderIdentityId",
+                table: "Profiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
