3af8a48342826015d5351c70c66c763b8be1a3d3
dylanperer <dylan.perer29@protonmail.com>
2023-10-23 16:03:00 +1300

    .

---
+++ Domain/Json/Gender.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrototypeBackend.Json;
+
+public struct Gender
+{
+    [Range(1, int.MaxValue)]
+    public int GenderIdentityId { get; set; }
+
+    public string Value  { get; set; }
+}
---
+++ Persistence/Migrations/20231023024447_t3k2asdaa5.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class t3k2asdaa5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "helloGenderIdentityId",
+                table: "Profiles",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_helloGenderIdentityId",
+                table: "Profiles",
+                column: "helloGenderIdentityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Profiles_Genders_helloGenderIdentityId",
+                table: "Profiles",
+                column: "helloGenderIdentityId",
+                principalTable: "Genders",
+                principalColumn: "GenderIdentityId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Profiles_Genders_helloGenderIdentityId",
+                table: "Profiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_helloGenderIdentityId",
+                table: "Profiles");
+
+            migrationBuilder.DropColumn(
+                name: "helloGenderIdentityId",
+                table: "Profiles");
+        }
+    }
+}
